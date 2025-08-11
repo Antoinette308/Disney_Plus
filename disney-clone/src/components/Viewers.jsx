0f9b5781a406bcabd4d61623fed1914392cea276
Antoinette Titre <antoinettetitre@hotmail.com>
2025-08-11 15:03:50 +0100

    Create components for home page: Recommends, New to Disney, Originals and Trending

--- disney-clone/src/components/Viewers.jsx
+++ disney-clone/src/components/Viewers.jsx
@@ -53,7 +53,7 @@ const Container = styled.div`
 const Wrap = styled.div`
     border-radius: 10px;
     box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
-        rgb(0 0 0 0 / 73%) 0px 16px 10px -10px;
+        rgb(0 0 0 / 73%) 0px 16px 10px -10px;
     cursor: pointer;
     overflow: hidden;
     position: relative;
